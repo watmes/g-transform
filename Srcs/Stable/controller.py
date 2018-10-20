174c51f2955241d1f56df72da3cf044ab6006626
osama <osama.ragab8504@icloud.com>
2018-10-20 11:40:16 +0200

    Final hackathon commit

--- Srcs/Stable/controller.py
+++ Srcs/Stable/controller.py
@@ -1,22 +1,32 @@
 #GOTTA FIX THESE DUMB MAGIC NUMBERS EVERYWHERE
 import findholes
+import strip
 import time
+import subprocess
 
 
-while (true):
-    execfile('strip.py')
+
+while (True):
+    strip.strip_f()
     bad_pixels = findholes.findholes_f()
     num_of_bad_pixels = len(bad_pixels)/2
     p = 2
     potential_hole = 0
     last_x = bad_pixels[1]
     last_y = bad_pixels[0]
+    num_of_lines = 1
     while ( p < num_of_bad_pixels-2 ):
-        if ((bad_pixels[p+1] == last_x+1) or (bad_pixels[p] == last_y+1)):
+        if (bad_pixels[p+1] == last_x+1):
             potential_hole += 1;
+            if potential_hole == 701:
+                print (" A hole was found!! Red Alert")
+        elif (bad_pixels[p] == last_y+1):
+            potential_hole += 1;
+            num_of_lines += 1
             if potential_hole == 701:
                 print (" A hole was found!! Red Alert")
         else:
             potential_hole = 0
+            num_of_lines = 1
         p += 2
     time.sleep(90)
--- Srcs/Stable/strip.py
+++ Srcs/Stable/strip.py
@@ -1,19 +1,18 @@
-#!/usr/local/bin/python
 import cv2
 
-image = cv2.imread('raw.jpg')
-
-
-
-g = image.copy()
-# set blue and red channels to 0
-g[:, :, 0] = 0
-g[:, :, 2] = 0
-
-
-
-# RGB - Green
-#cv2.imshow('G-RGB', g)
-cv2.imwrite('stripped.jpg', g)
-
-cv2.waitKey(0)
+def strip_f():
+    image = cv2.imread('raw.jpg')
+    
+    
+    
+    g = image.copy()
+    # set blue and red channels to 0
+    g[:, :, 0] = 0
+    g[:, :, 2] = 0
+    
+    
+    
+    # RGB - Green
+    #cv2.imshow('G-RGB', g)
+    cv2.imwrite('stripped.jpg', g)
+    return
